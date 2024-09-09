a105aa4cfc45ff806b331b474a091a2bdaa25eda
Yulia Starchenko <imay@amake.ru>
2024-09-09 22:39:28 +0300

    refactor: used each on task 2

--- task2/task2.test.ts
+++ task2/task2.test.ts
@@ -1,51 +1,20 @@
-import {isValid} from "./task2";
+import { isValid } from "./task2";
 
 describe('task 2 valid parentheses', () => {
-  test('() should return true', () => {
-    expect(isValid('()')).toBe(true);
+  test.each([
+    { data: '()', result: true },
+    { data: '{}', result: true },
+    { data: '()[]{}', result: true },
+    { data: '({()})([]{})', result: true },
+    { data: '((())())', result: true },
+    { data: ')', result: false },
+    { data: '(]', result: false },
+    { data: '(}', result: false },
+    { data: ')(){', result: false },
+    { data: '(){[]', result: false },
+    { data: '(){[])', result: false },
+    { data: '(((((){}))', result: false },
+  ])('should return the correct result', ({ data, result }) => {
+    expect(isValid(data)).toBe(result);
   });
-
-  test('{} should return true', () => {
-    expect(isValid('{}')).toBe(true);
-  });
-
-  test('()[]{} should return true', () => {
-    expect(isValid('()[]{}')).toBe(true);
-  });
-
-  test('({()})([]{}) should return true', () => {
-    expect(isValid('({()})([]{})')).toBe(true);
-  });
-
-  test('(())()) should return true', () => {
-    expect(isValid('((())())')).toBe(true);
-  });
-
-  test(') should return false', () => {
-    expect(isValid(')')).toBe(false);
-  });
-
-  test('(] should return false', () => {
-    expect(isValid('(]')).toBe(false);
-  });
-
-  test('(} should return false', () => {
-    expect(isValid('(}')).toBe(false);
-  });
-
-  test(')(){ should return false', () => {
-    expect(isValid(')(){')).toBe(false);
-  });
-
-  test('(){[] should return false', () => {
-    expect(isValid('(){[]')).toBe(false);
-  });
-
-  test('(){[]) should return false', () => {
-    expect(isValid('(){[])')).toBe(false);
-  });
-
-  test('(((((){})) should return false', () => {
-    expect(isValid('(((((){}))')).toBe(false);
-  });
-})
+});
